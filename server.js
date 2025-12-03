bb228524fb42b274f2bbe07891cfb43f3de28b01
PORO <d89497753j@gmail.com>
2025-12-03 17:44:40 +0530

    Add health check endpoint to server

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("Backend is LIVE 🚀");
+});
 const upload = multer({ dest: "uploads/" });
 
 // SET THESE 👇
